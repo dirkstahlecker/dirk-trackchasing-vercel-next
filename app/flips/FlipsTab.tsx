a3a14a82327f7493af3e9116cd1e998fcf9767b4
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2026-08-30 21:45:47 -0400

    restored flip notes

--- app/flips/FlipsTab.tsx
+++ app/flips/FlipsTab.tsx
@@ -3,13 +3,32 @@
 import React from "react"
 import { FlipsData, TrackVideo } from "./FlipData"
 
-//flip videos are on TrackchaserDirk youtube account
-
+// flip videos are on TrackchaserDirk YouTube account
 export default function FlipsTab() {
-  return FlipsData.map((value: TrackVideo) => {
-    return <React.Fragment key={value.url + value.num}>
-      <a href={value.url} target="_blank">{value.num}: {value.track}</a>
-      <br/>
-    </React.Fragment>
-  })
+  return (
+    <div>
+      <table style={{ margin: "0 auto", textAlign: "center" }}>
+        <thead>
+          <tr>
+            <th>Num</th>
+            <th>Track</th>
+            <th>Notes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {FlipsData.map((value: TrackVideo) => (
+            <tr key={value.url + value.num}>
+              <td>{value.num}</td>
+              <td>
+                <a href={value.url} target="_blank" rel="noopener noreferrer">
+                  {value.track}
+                </a>
+              </td>
+              <td>{value.notes}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
